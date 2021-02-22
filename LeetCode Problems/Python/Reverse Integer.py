aee3fa67d1e500ce0f196ae300e54b0d70fb6f66
AbhiByte <dbossgames20gmail.com>
2021-02-21 21:09:46 -0500

    Finshed 7. Reverse Integer

--- LeetCode Problems/Python/Reverse Integer.py
+++ LeetCode Problems/Python/Reverse Integer.py
@@ -1,4 +1,20 @@
-class Solution(object):
-    def reverse(self, x):
-        for i in range len(x):
-            print(x)
+# 7. Reverse Integer
+def reverseInt(x):
+    x = str(x)
+    if x[-1] != "0" and x[0] != "-":
+        x = x[::-1]
+    elif x[-1] == "0" and x[0] != '-':
+        x = x[::-1]
+    elif x[-1] == "0" and x[0] == "-":
+        x = x[1::]
+        x = x[::-1]
+        x = x[1::]
+        x = "-" + x
+    elif x[0] == "-":
+        x = x[1:]
+        x = x[::-1]
+        x = "-" + x
+    x = int(x)
+    if x > 2**31 - 1 or x < -1 * (2**31):
+        x = 0
+    return x
