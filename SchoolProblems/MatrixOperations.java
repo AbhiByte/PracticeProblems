73a414dadc09e4cf192b26dd477fed514f1eaa77
AbhiByte <dbossgames20gmail.com>
2021-02-22 13:16:05 -0500

    Woah

--- SchoolProblems/MatrixOperations.java
+++ SchoolProblems/MatrixOperations.java
@@ -1,9 +1,8 @@
-
 public class MatrixOperations {
 
 	public static void main(String[] args) {
-        int[][]matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-		int[][]matrix2 =  {{2, 3, 5}, {7, 11, 13}, {17, 19, 23}};
+        int[][]matrix1 = {{1, 2, 3, 4}, {4, 5, 6, 7}, {7, 8, 9, 10}};
+		int[][]matrix2 =  {{2, 3, 5}, {7, 11, 13}, {17, 19, 23}, {3, 1, 4}};
 
 		PrintArray.print(matrix1);
 		PrintArray.print(matrix2);
@@ -13,20 +12,20 @@ public class MatrixOperations {
 public static int[][] productMatrix(int[][]matrixA, int[][]matrixB)
 	{
 
-	int row1 = matrixA.length;
+		int row1 = matrixA.length;
 
-	int col1= matrixA[0].length;
+		int col1= matrixA[0].length;
 
-	int row2 = matrixB.length;
+		int row2 = matrixB.length;
 
-	int col2 = matrixB[0].length;
+		int col2 = matrixB[0].length;
 
-	int[][] finalProduct = new int [row1][col2];
+		int[][] finalProduct = new int [row1][col2];
 
-	if (row2 != col1)
-	{
+		if (row2 != col1)
+		{
 		System.out.println("Matrices not compatible");
-	}
+		}
 
 		for (int i = 0; i < row1; i++)
 		{
