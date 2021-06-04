85458aeefab110944e70e10b8285e687c5e81af7
Abhinav Ramesh <64025335+AbhiByte@users.noreply.github.com>
2021-06-03 23:01:25 -0400

    Added comments

--- LeetCode Problems/Python/771 Jewels and Stones.py
+++ LeetCode Problems/Python/771 Jewels and Stones.py
@@ -1,4 +1,5 @@
-
+#Complexity could be improved
+#For each element in stones checks to see if element in jewels also in stones and ++ the counter
 def numJewelsInStones(jewels, stones):
     counter = 0
     for x in stones:
