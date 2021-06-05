91df88bb318294ee1897d0913a4eb5e2e7060c5d
AbhiByte <dbossgames20gmail.com>
2021-06-05 11:49:09 -0400

    changes

--- LeetCode Problems/Python/1828 Queries on Number of Points Inside a Circle.py
+++ LeetCode Problems/Python/1828 Queries on Number of Points Inside a Circle.py
@@ -1,4 +1,4 @@
-import math
+
 def countPoints(points, queries):
     radii = []
     ans = []
@@ -6,10 +6,11 @@ def countPoints(points, queries):
     for i in range(len(queries)):
         for j in range(len(queries[i])):
             if j == 0 and j+1 == 1:
-                for x in range(len(points)):
-                    for y in range(len(points[x])):
-                        if y == 0 and y+1 == 1:
-                            print(points[x][y], points[x][y+1])
+
+    for x in range(len(points)):
+        for y in range(len(points[x])):
+            if y == 0 and y+1 == 1:
+
 
             if j == 2:
                 radii.append(queries[i][j])
