6439a06c67e5d6b5bbe3f2a1b0b386dfc7aa472b
AbhiByte <dbossgames20gmail.com>
2021-06-09 22:16:42 -0400

    Almost done

---
+++ Google Kickstart/2020/AP1.py
@@ -0,0 +1,7 @@
+#First line of input is number of test cases
+
+num_cases = input()
+for x in range(len(num_cases)):
+    #Taking multiple inputs in same line
+    N, B = [int(x) for x in input().split()]
+    N_list = input().split()
