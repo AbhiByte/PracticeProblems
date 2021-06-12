f694fc47d0af350e39560025e6a5ec90e4fa94a6
AbhiByte <dbossgames20@gmail.com>
2021-06-11 22:49:01 -0400

    Started working on medium problem.

---
+++ LeetCode Problems/Python/1769 Minimum Number of Operations to Move All Balls to Each Box.py
@@ -0,0 +1,12 @@
+def minOperations(boxes):
+        """
+        :type boxes: str
+        :rtype: List[int]
+        """
+boxes = "110"
+
+"""
+expected ans: [1, 1, 2]
+1 move to move all balls to box 1. 1 move to move all balls to box 2
+2 moves to move all balls to box 3
+"""
