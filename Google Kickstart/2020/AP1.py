35fd84afdeae60daf311d4be7ed648a9cabf83c4
AbhiByte <dbossgames20gmail.com>
2021-06-11 21:05:03 -0400

    hi

--- Google Kickstart/2020/AP1.py
+++ Google Kickstart/2020/AP1.py
@@ -2,6 +2,14 @@
 
 num_cases = input()
 for x in range(len(num_cases)):
+
     #Taking multiple inputs in same line
     N, B = [int(x) for x in input().split()]
     N_list = input().split()
+    temp_ans = 0
+    #
+    for i in range(len(N_list)):
+        for j in range(len(N_list)+1):
+            if i+j < B:
+                temp_ans += 1
+    return temp_ans
