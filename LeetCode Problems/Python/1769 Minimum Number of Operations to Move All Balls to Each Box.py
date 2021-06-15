79468f269e0f355f2d328256c34fccfeec7ca056
AbhiByte <dbossgames20@gmail.com>
2021-06-14 21:43:53 -0400

    Made good progress on leetcode medium problem but not done. Logic error

--- LeetCode Problems/Python/1769 Minimum Number of Operations to Move All Balls to Each Box.py
+++ LeetCode Problems/Python/1769 Minimum Number of Operations to Move All Balls to Each Box.py
@@ -1,15 +1,29 @@
 def minOperations(boxes):
-        ans = map(int, boxes)
-        print(list(ans)[0])
+    ans = []
+    #Right side, Left side
+    for x in range(len(boxes)):
+        right_count, left_count = 0, 0
+        for y in range(len(boxes) + 1):
+            if boxes[x] == '1':
+                right_count += (y-x)
+
+        for y in range(x-1):
+            if y > -1 and y != x:
+                left_count += (x-y)
+
+
+        ans.append(right_count+left_count)
+    return ans
+
 
 
 boxes = "110"
-minOperations(boxes)
+print(minOperations(boxes))
 
 
 
 """
-expected ans: [1, 1, 2]
+expected ans: [1, 1, 3]
 1 move to move all balls to box 1. 1 move to move all balls to box 2
-2 moves to move all balls to box 3
+3 moves to move all balls to box 3
 """
