fa0117ca346e7657ba9260c83e3391df6308fee2
AbhiByte <dbossgames20@gmail.com>
2021-06-21 10:48:09 -0400

    Solved problem using hash map

--- LeetCode Problems/Python/FirstNonRepeatingCharacter.py
+++ LeetCode Problems/Python/FirstNonRepeatingCharacter.py
@@ -1,2 +1,13 @@
 #Going to use dictionary to get O(n) time
+#Given a string find the first non repeating character
+
 def answer(string, map={}):
+    #Creates dictionary
+    for x in string:
+        map[x] = map.setdefault(x, 0) + 1
+    #Searches dictionary for character with 'character' : 1
+    for x in map:
+        if map[x] == 1:
+            return x
+    #return map
+print(answer("aaabcbbcefff"))
