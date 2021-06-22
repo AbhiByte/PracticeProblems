b76631abb740fe6e740dac096bdfaee5d5fea3a3
AbhiByte <dbossgames20@gmail.com>
2021-06-22 17:44:07 -0400

    .

--- LeetCode Problems/Python/14 Longest Common Prefix.py
+++
@@ -1,6 +0,0 @@
-def prefix (list):
-    for x in len(list):
-        print(x)
-
-myList = ["Hello", "Hi", "Whats", "Up"]
-prefix(myList)
---
+++ LeetCode Problems/Python/14. Longest Common Prefix.py
@@ -0,0 +1,11 @@
+lst = ['hello', 'hell', 'hehe']
+
+ans = []
+for item in lst:
+    new_item = list(item)
+    ans.append(new_item)
+print(ans)
+
+
+for i in ans:
+    for j in ans[i]:
