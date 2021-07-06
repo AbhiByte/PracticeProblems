2b18cb94c99db387da4559ea863d3a66a2524fd5
AbhiByte <dbossgames20@gmail.com>
2021-07-05 22:34:58 -0400

    Solved

--- Codewars/rotthirteen.py
+++ Codewars/rotthirteen.py
@@ -3,9 +3,12 @@ def rot13(message):
     inp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
 
-    for element in message:
-        if element.isalpha() == True:
-            message.replace(element, output[inp.find(element)])
-    return message
+    mess_lst = list(message)
+    for i, item in enumerate(mess_lst):
+        if item.isalpha() == True:
+            mess_lst[i] = output[inp.find(item)]
+    return ''.join(mess_lst)
+
+
 
 print(rot13("EBG13 rknzcyr."))
