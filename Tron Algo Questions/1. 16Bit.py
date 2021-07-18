815c27f7136e4024ac40d17c993e64aa917c839d
AbhiByte <dbossgames20@gmail.com>
2021-07-18 11:15:31 -0400

    Done

--- Tron Algo Questions/1. 16Bit.py
+++ Tron Algo Questions/1. 16Bit.py
@@ -1,3 +1,5 @@
+'''Q1. Iterate through str(num) and count number of '1'.
+O(n) because iterate through num once'''
 def high_bits(num):
     count = 0
     for x in str(num):
---
+++ Tron Algo Questions/2. EvenOrOdd.py
@@ -0,0 +1,5 @@
+'''Q2. O(1) solution because 1 check of num'''
+def even_or_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    return "Odd"
