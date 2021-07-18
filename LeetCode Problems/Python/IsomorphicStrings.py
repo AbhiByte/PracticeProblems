123e7f17c7d974a88132dd32aad46eb82a91f727
AbhiByte <dbossgames20@gmail.com>
2021-07-18 11:25:05 -0400

    .

--- LeetCode Problems/Python/IsomorphicStrings.py
+++ LeetCode Problems/Python/IsomorphicStrings.py
@@ -1,23 +1,9 @@
 import itertools
 
 def isIsomorphic(s, t):
-    if len(s) != len(t):
-        return False
     pairs = {}
     used = {}
-
-    for i in range(len(s)):
-        char1 = s[i]
-        char2 = t[i]
-        if char1 in pairs.keys():
-            if pairs[char1] != char2:
-                return False
-        else:
-            if used[char2] == True:
-                return False
-            else:
-                pairs[char1] = char2
-                used[char2] = True
+    
 
 
 
