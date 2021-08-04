53a6d5b8e60806d0f6604035806cc39981eda15d
AbhiByte <dbossgames20@gmail.com>
2021-08-04 18:15:07 -0400

    Done

---
+++ Tron Algo Questions/13. Missing Nums.py
@@ -0,0 +1,11 @@
+def missing(arr):
+    MAX = max(arr)
+    count = min(arr)
+    ans = []
+    for x in range(MAX):
+        if count not in arr:
+            ans.append(count)
+        count+=1
+    return ans
+
+print(missing([1,3,5]))
