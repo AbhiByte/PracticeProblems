016337ebeeab1c1b9b55073673c2bc0d2a9a25fa
Abhinav Ramesh <64025335+AbhiByte@users.noreply.github.com>
2022-01-18 11:19:19 -0500

    Added solution

--- Tron Algo Questions/6. Flatten Array.py
+++ Tron Algo Questions/6. Flatten Array.py
@@ -8,3 +8,19 @@ def flatten(arr):
         return [arr]
 
 print(flatten([1, 2, [[3], [4, 5], [], 6], 7, [[[8]]]]))
+
+
+
+#My solution - Not done yet
+def flat(arr):
+    string = ''.join(str(e) for e in arr)
+    print(string)
+    
+    decrypted = [x for x in string]
+    
+    for char in decrypted:
+        if char.isdigit() == False:
+            decrypted.remove(char)
+    print(decrypted)        
+            
+flat([1, 2, [[3], [4, 5], [], 6], 7, [[[8]]]])
