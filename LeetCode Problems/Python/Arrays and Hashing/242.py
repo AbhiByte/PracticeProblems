3bfac9e0969b30fb3bf44c9148f812766b71b922
Abhinav Ramesh <64025335+AbhiByte@users.noreply.github.com>
2022-09-12 02:19:21 +0530

    Update 242.py

--- LeetCode Problems/Python/Arrays and Hashing/242.py
+++ LeetCode Problems/Python/Arrays and Hashing/242.py
@@ -14,4 +14,27 @@ class Solution:
         if a==b:
             return True
         return False
+
+'''Runtime: 111 ms, faster than 13.70% of Python3 online submissions for Valid Anagram.
+Memory Usage: 15.2 MB, less than 12.03% of Python3 online submissions for Valid Anagram.'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+    
+        #Method 2 -> Hashing
+        count1 = {}
+        count2 = {}
+        ls = [char for char in s]
+        lt = [char for char in t]
+        
+        for x in ls:
+            count1.setdefault(x, 0)
+            count1[x] += 1
+        
+        for x in lt:
+            count2.setdefault(x, 0)
+            count2[x] += 1
+        
+        if count1 == count2:
+            return True
+        return False
  
