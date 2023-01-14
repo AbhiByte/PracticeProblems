6de60ecf34332eded4c11a95dc35aa58d763a741
Abhinav Ramesh <64025335+AbhiByte@users.noreply.github.com>
2023-01-14 17:45:27 -0500

    Update ContainsDuplicate.py

--- NeetCode 150/Arrays and Hashing/ContainsDuplicate.py
+++ NeetCode 150/Arrays and Hashing/ContainsDuplicate.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = {}
+        for num in nums:
+            count.setdefault(num, 0)
+            count[num] += 1
+        
+        for key, value in count.items():
+            if value >= 2:
+                return True
+        return False
+ 
