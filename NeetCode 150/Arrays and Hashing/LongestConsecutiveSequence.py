3c86d4195ea2a05fabdc9dad8682a77c734541ed
Abhinav Ramesh <64025335+AbhiByte@users.noreply.github.com>
2023-02-08 20:44:19 -0500

    Update LongestConsecutiveSequence.py

--- NeetCode 150/Arrays and Hashing/LongestConsecutiveSequence.py
+++ NeetCode 150/Arrays and Hashing/LongestConsecutiveSequence.py
@@ -1,12 +1,10 @@
 #My solution. Run O(n) since at most one iteration through nums. Uses set which adds memory
-def remove_duplicates(l):
-    return list(set(l))
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums = remove_duplicates(nums)
+        nums = list(set(nums)).sort()
         if len(nums) == 0:
             return 0
-        nums = sorted(nums)
         count = 1
         seq = []
         l = 0
@@ -23,3 +21,19 @@ class Solution:
                 r+=1
         seq.append(count)     
         return max(seq)
+    
+    
+    #NC Soluton. Linear time, memory. Similar to mine but uses exact value instead of index
+    class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            # check if its the start of a sequence
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
