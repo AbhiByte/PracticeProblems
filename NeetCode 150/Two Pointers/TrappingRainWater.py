80e04e1a7620322db1f1985d9f6c6fa06e31ab09
Abhinav Ramesh <64025335+AbhiByte@users.noreply.github.com>
2023-03-15 15:01:36 -0400

    Update TrappingRainWater.py

--- NeetCode 150/Two Pointers/TrappingRainWater.py
+++ NeetCode 150/Two Pointers/TrappingRainWater.py
@@ -10,4 +10,23 @@ class Solution:
                 water += calc
         return water
       
-#optimal Solution
+#optimal Solution (with help from NC)
+#O(n) time and constant space. Revisit after some time for refresher 
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height)-1
+        maxLeft, maxRight = height[l], height[r]
+        output = 0
+
+        while l < r:
+            if maxLeft < maxRight:
+                l+=1
+                maxLeft = max(maxLeft, height[l])
+                output += maxLeft - height[l]
+            
+            else:
+                r-=1
+                maxRight = max(maxRight, height[r])
+                output += maxRight - height[r]
+            
+        return output
