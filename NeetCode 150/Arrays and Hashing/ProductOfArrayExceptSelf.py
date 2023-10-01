8bce8039dfc7764b24232beed19926b9645828c6
Abhinav Ramesh <dbossgames20@gmail.com>
2023-09-30 23:18:50 -0400

    add msg to productofarrexceptself

--- NeetCode 150/Arrays and Hashing/ProductOfArrayExceptSelf.py
+++ NeetCode 150/Arrays and Hashing/ProductOfArrayExceptSelf.py
@@ -1,16 +1,20 @@
-'''Solution that works but doesnt pass on LC due to runtime error'''
+"""Solution that works but doesnt pass on LC due to runtime error"""
 import math
+
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = [1]*len(nums)
+        output = [1] * len(nums)
         for i in range(len(nums)):
             prefix_temp = [num for num in nums[:i]]
-            postfix_temp = [num for num in nums[i+1:]]
+            postfix_temp = [num for num in nums[i + 1 :]]
             output[i] = math.prod(prefix_temp) * math.prod(postfix_temp)
         return output
-      
-      
-'''Neetcode solution'''
+
+
+"""Neetcode solution Early 2023"""
+
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * (len(nums))
@@ -24,3 +28,6 @@ class Solution:
             res[i] *= postfix
             postfix *= nums[i]
         return res
+
+
+"""Fall 2023 I had the same solution as above. O(2n) since 2 passes through array"""
