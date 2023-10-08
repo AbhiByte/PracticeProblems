48a0b99053cf18312db8e734cc5e5377b47914f7
Abhinav Ramesh <dbossgames20@gmail.com>
2023-10-07 21:09:15 -0400

    redid ispalindrome

--- NeetCode 150/Two Pointers/ValidPalindrome.py
+++ NeetCode 150/Two Pointers/ValidPalindrome.py
@@ -1,7 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
         s = "".join(ch for ch in s if ch.isalnum()).lower()
-        if s == s[::-1]:
-            return True
-        return False
+        return s == s[::-1]
+
+
+# Fall 2023 solution.
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        for char in s:
+            if char.isalnum() == False:
+                s = s.replace(char, "")
+        return s.lower() == s[::-1].lower()
