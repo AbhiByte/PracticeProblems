22fcf92fa35bf35f0dea155ad606cb5f32c7fb06
Abhinav Ramesh <dbossgames20@gmail.com>
2023-10-19 21:40:13 -0400

    finished 2 sum_2

--- NeetCode 150/Two Pointers/TwoSum_2.py
+++ NeetCode 150/Two Pointers/TwoSum_2.py
@@ -1,11 +1,29 @@
-#Medium problem similar to TwoSum but index+1
+# Medium problem similar to TwoSum but index+1
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
         output = []
         val_index = {}
         for i in range(len(numbers)):
             diff = target - numbers[i]
             if diff in val_index.keys():
-                return [val_index[diff] + 1, i+1]
+                return [val_index[diff] + 1, i + 1]
             else:
                 val_index.setdefault(numbers[i], i)
+
+
+# Fall 2023 solution
+# Time complexity O(n), space complexity constant
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curr_sum = numbers[l] + numbers[r]
+            if curr_sum == target:
+                break
+            elif curr_sum < target:
+                l += 1
+            else:
+                r -= 1
+
+        return sorted([l + 1, r + 1])
