2e29807c9e2f787bf5118773a60876c87e84b04e
Abhinav Ramesh <dbossgames20@gmail.com>
2023-10-21 16:00:54 -0400

    complete 3sum

--- NeetCode 150/Two Pointers/3sum.py
+++ NeetCode 150/Two Pointers/3sum.py
@@ -1,4 +1,4 @@
-#My solution. Works but runtime error :/
+# My solution. Works but runtime error :/
 def twoSum(nums, target, index3sum):
     result = []
     prevMap = {}  # val -> index
@@ -10,20 +10,48 @@ def twoSum(nums, target, index3sum):
             prevMap[n] = i
     return result
 
+
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         sol = []
         for i in range(len(nums)):
             target = -nums[i]
-            newList = nums[:i] + nums[i+1:]
+            newlist = nums[:i] + nums[i + 1 :]
             twoSumResult = twoSum(nums, target, i)
-            #print(f"nums[{i}]: {nums[i]}, target: {target}, newList: {newList}, twoSumResult: {twoSumResult}")
+            # print(f"nums[{i}]: {nums[i]}, target: {target}, newlist: {newlist}, twoSumResult: {twoSumResult}")
             if twoSumResult != None:
                 for lst in twoSumResult:
                     sol.append([nums[i]] + [nums[lst[0]]] + [nums[lst[1]]])
-        
+
         seen = []
         for lst in sol:
             if sorted(lst) not in seen:
                 seen.append(sorted(lst))
         return seen
+
+
+# Fall 2023 solution (not mine)
+def threeSum(nums: list[int]) -> list[list[int]]:
+    nums.sort()
+    output = []
+
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:  # if repeat number, move on
+            continue
+
+        l, r = i + 1, len(nums) - 1  # conduct 2 sum II on the remaining list
+
+        while l < r:
+            three_sum = a + nums[l] + nums[r]
+
+            if three_sum > 0:
+                r -= 1
+            elif three_sum < 0:
+                l += 1
+            else:
+                output.append([a, nums[r], nums[l]])
+
+                l += 1  # while repeat num, move left pointer (or we could move right pointer)
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+    return output
