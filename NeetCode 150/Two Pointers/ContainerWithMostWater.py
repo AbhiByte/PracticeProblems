a384a06e6812d1abca0a172d345d78f8012cef4b
Abhinav Ramesh <dbossgames20@gmail.com>
2023-10-21 16:13:37 -0400

    complete contained with most water

--- NeetCode 150/Two Pointers/ContainerWithMostWater.py
+++ NeetCode 150/Two Pointers/ContainerWithMostWater.py
@@ -1,15 +1,34 @@
-#My solution
+# My solution
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
+    def maxArea(self, height: list[int]) -> int:
         maxArea = 0
 
-        l, r = 0, len(height)-1
-        while l<r:
-            area = min(height[r], height[l]) * (r-l)
+        l, r = 0, len(height) - 1
+        while l < r:
+            area = min(height[r], height[l]) * (r - l)
             maxArea = max(maxArea, area)
-            
+
             if height[l] < height[r]:
-                l+=1
+                l += 1
             elif height[r] <= height[l]:
-                r-=1
+                r -= 1
+        return maxArea
+
+
+# Fall 2023 Solution
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        l, r = 0, len(height) - 1
+        maxArea = 0
+        while l < r:
+            area = (r - l) * min(height[r], height[l])
+
+            maxArea = max(area, maxArea)
+
+            if height[l] < height[r]:
+                l += 1
+            elif height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
         return maxArea
