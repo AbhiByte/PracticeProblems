798f87bcc3caeaacfa9c025958a7b1deb1bc0a50
Abhinav Ramesh <dbossgames20@gmail.com>
2023-11-05 18:32:13 -0500

    solve some problems

--- NeetCode 150/Stack/EvaluateReversePolishNotation.py
+++ NeetCode 150/Stack/EvaluateReversePolishNotation.py
@@ -1,26 +1,64 @@
-#O(n) solution with stack. 
+# O(n) solution with stack.
+
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         operators = ["+", "-", "*", "/"]
-        
+
         for token in tokens:
             if token == "+":
                 stack.append(stack.pop() + stack.pop())
-            
+
             elif token == "-":
-                stack.append(-1*(stack.pop() - stack.pop()))
-            
+                stack.append(-1 * (stack.pop() - stack.pop()))
+
             elif token == "*":
                 stack.append(stack.pop() * stack.pop())
-            
+
             elif token == "/":
                 prev = stack.pop()
                 prev_prev = stack.pop()
-                stack.append(int(prev_prev/prev))
-            
+                stack.append(int(prev_prev / prev))
+
             else:
                 stack.append(int(token))
-        
+
         return stack[0]
+
+
+# Fall 2023 Solution. Similar to above but not optimized:
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token == "+":
+                a, b = int(stack[-2]), int(stack[-1])
+                res = a + b
+                stack.pop()
+                stack.pop()
+                stack.append(res)
+            elif token == "-":
+                a, b = int(stack[-2]), int(stack[-1])
+                res = a - b
+                stack.pop()
+                stack.pop()
+                stack.append(res)
+            elif token == "*":
+                a, b = int(stack[-1]), int(stack[-2])
+                res = a * b
+                stack.pop()
+                stack.pop()
+                stack.append(res)
+            elif token == "/":
+                a, b = int(stack[-2]), int(stack[-1])
+                res = int(a / b)
+                stack.pop()
+                stack.pop()
+                stack.append(res)
+            else:
+                stack.append(token)
+
+        return int(stack[0])
--- NeetCode 150/Stack/MinStack.py
+++ NeetCode 150/Stack/MinStack.py
@@ -1,17 +1,15 @@
-#Obviosuly using .pop() in MinStack.pop() defeats the purpose
+# Obviosuly using .pop() in MinStack.pop() defeats the purpose
 class MinStack:
-
     def __init__(self):
-        self.stack = []    
+        self.stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        
 
     def pop(self) -> None:
-        #self.stack.pop()
-        '''Or we can use del'''
-        del(self.stack[len(self.stack)-1])
+        # self.stack.pop()
+        """Or we can use del"""
+        del self.stack[len(self.stack) - 1]
 
     def top(self) -> int:
         return self.stack[-1]
@@ -28,3 +26,28 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+
+# Fall 2023 solution
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # O(1)
+
+    def pop(self) -> None:
+        del self.stack[-1]
+        # O(1)
+
+    def top(self) -> int:
+        if not self.stack:
+            return None
+        item = self.stack[-1]
+        del self.stack[-1]
+        return item
+        # O(1)
+
+    def getMin(self) -> int:
+        return min(self.stack, default=-1)
