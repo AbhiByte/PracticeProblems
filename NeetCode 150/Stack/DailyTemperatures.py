3a6c59a7084d4862a51e2602a97c4157d0442483
Abhinav Ramesh <dbossgames20@gmail.com>
2023-11-11 20:14:34 -0500

    finish daily temperatures

--- NeetCode 150/Stack/DailyTemperatures.py
+++ NeetCode 150/Stack/DailyTemperatures.py
@@ -1,34 +1,57 @@
-'''
+"""
 Brute force solution
 Time complexity O(n^2)
-'''
-def dailyTempBruteForce(temperatures):
-  ans = []
-  for i in range(len(temperatures)):
-      if len(ans) < i:
-          ans.append(0)
-      if i == len(temperatures) - 1:
-          ans.append(0)
-      for j in range(i+1, len(temperatures)):
-          if temperatures[j] > temperatures[i]:
-              ans.append(j-i)
-              break
-  return ans
+"""
 
-'''
+
+def dailyTempBruteForce(temperatures):
+    ans = []
+    for i in range(len(temperatures)):
+        if len(ans) < i:
+            ans.append(0)
+        if i == len(temperatures) - 1:
+            ans.append(0)
+        for j in range(i + 1, len(temperatures)):
+            if temperatures[j] > temperatures[i]:
+                ans.append(j - i)
+                break
+    return ans
+
+
+"""
 Optimal solution O(n)
-'''
+"""
+
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = []
-        ans = [0]*len(temperatures)
+        ans = [0] * len(temperatures)
 
-        
         for index, temp in enumerate(temperatures):
             while stack and temp > stack[-1][1]:
                 ans[stack[-1][0]] = index - stack[-1][0]
                 stack.pop()
             stack.append([index, temp])
-            
+
         return ans
+
+
+# Fall 2023 solution
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+
+        # For each element in temperatures
+        for i, temp in enumerate(temperatures):
+            # While there are elements in stack and the curr temperature is greater than
+            # the temperature in the stack
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                # pop the latest index from the stack and update res
+                prev_index = stack.pop()
+                res[prev_index] = i - prev_index
+
+            stack.append(i)
+
+        return res
