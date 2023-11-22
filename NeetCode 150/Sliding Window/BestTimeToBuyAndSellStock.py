c59b981f3866c317eba4525bdb46dea2657237c8
Abhinav Ramesh <dbossgames20@gmail.com>
2023-11-21 21:58:14 -0500

    add some solutions

--- NeetCode 150/Sliding Window/BestTimeToBuyAndSellStock.py
+++ NeetCode 150/Sliding Window/BestTimeToBuyAndSellStock.py
@@ -1,14 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        l, r = 0, 1 #Initial window
+        l, r = 0, 1  # Initial window
         maxProfit = 0
-        
+
         while r < len(prices):
-            curr = prices[r] - prices[l] #Current price
-            if prices[l] < prices[r]:    #If the transanction is +ve
-                maxProfit = max(curr, maxProfit) #Take the max of the curr and prev
+            curr = prices[r] - prices[l]  # Current price
+            if prices[l] < prices[r]:  # If the transanction is +ve
+                maxProfit = max(curr, maxProfit)  # Take the max of the curr and prev
             else:
-                l = r                    #Otherwise, move left window to right
-            r += 1                       #And increment right window by 1
+                l = r  # Otherwise, move left window to right
+            r += 1  # And increment right window by 1
+        return maxProfit
+
+
+# Fall 2023 solution
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxProfit = 0
+        while r < len(prices):
+            maxProfit = max(maxProfit, (prices[r] - prices[l]))
+
+            if prices[r] <= prices[l]:
+                l = r
+                r = l + 1
+            else:
+                r += 1
         return maxProfit
