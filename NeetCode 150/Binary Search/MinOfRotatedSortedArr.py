55f8c1abd6aba6dd8375d6435a91f8d2cbc43a66
Abhinav Ramesh <dbossgames20@gmail.com>
2023-11-21 21:34:16 -0500

    add sol

--- NeetCode 150/Binary Search/MinOfRotatedSortedArr.py
+++ NeetCode 150/Binary Search/MinOfRotatedSortedArr.py
@@ -1,14 +1,28 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums)-1
-        #We know initially l > r because it is rotated
+        l, r = 0, len(nums) - 1
+        # We know initially l > r because it is rotated
         minimum = nums[0]
-        while l<=r:
-            mid = (l+r) // 2
+        while l <= r:
+            mid = (l + r) // 2
             minimum = min(minimum, nums[mid])
 
             if nums[mid] > nums[r]:
                 l = mid + 1
             else:
-                r = mid - 1 
+                r = mid - 1
         return min(minimum, nums[l])
+
+
+# My Fall 2023 solution
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[l] <= nums[mid] <= nums[r]:
+                return nums[l]
+            elif nums[mid] >= nums[r]:
+                l = mid + 1
+            else:
+                r = mid
