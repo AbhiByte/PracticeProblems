967e2236e8513cb86f1c7b367709ab286d8dc595
Abhinav Ramesh <dbossgames20@gmail.com>
2024-06-27 07:46:23 -0400

    prod of arr except self: new solution

--- NeetCode 150/Arrays and Hashing/ProductOfArrayExceptSelf.py
+++ NeetCode 150/Arrays and Hashing/ProductOfArrayExceptSelf.py
@@ -3,7 +3,7 @@ import math
 
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
         output = [1] * len(nums)
         for i in range(len(nums)):
             prefix_temp = [num for num in nums[:i]]
@@ -16,7 +16,7 @@ class Solution:
 
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
         res = [1] * (len(nums))
 
         prefix = 1
@@ -31,3 +31,23 @@ class Solution:
 
 
 """Fall 2023 I had the same solution as above. O(2n) since 2 passes through array"""
+
+
+# Summer 2024 solution
+def productExceptSelf(nums: list[int]) -> list[int]:
+
+        left = [1]*len(nums)
+        prod = 1
+        for i, num in enumerate(nums[:len(nums)-1]):
+            prod *= num
+            left[i+1] = prod
+
+        prod = 1
+        right = [1]*len(nums)
+
+        for i, num in enumerate(nums[::-1]):
+            prod *= num
+            right[len(nums) - i - 2] *= prod
+            right[-1] = 1
+
+        return [l*r for l, r in zip(left, right)]
