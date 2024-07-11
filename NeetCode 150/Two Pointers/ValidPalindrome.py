067eae76d4273d3b2be4f818e86895d63f4413bc
Abhinav Ramesh <dbossgames20@gmail.com>
2024-07-11 18:51:04 -0400

    add sol

--- NeetCode 150/Two Pointers/ValidPalindrome.py
+++ NeetCode 150/Two Pointers/ValidPalindrome.py
@@ -11,3 +11,23 @@ class Solution:
             if char.isalnum() == False:
                 s = s.replace(char, "")
         return s.lower() == s[::-1].lower()
+    
+    
+    
+# Summer 2024 solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(char for char in s if char.isalnum()).lower()
+
+
+        # O(n/2) = O(n)
+        for i in range((len(s) + 1) // 2):  # Only need to go halfway
+            front_char = s[i]
+            back_char = s[-(i + 1)]
+
+            if front_char != back_char:
+                return False
+        
+        return True
+                
+        
