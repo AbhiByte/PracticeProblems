599c1bd9d15d903f9f1d37e1a42540b963023e41
Abhinav Ramesh <dbossgames20@gmail.com>
2024-07-20 11:08:19 -0400

    summer 2024 3sum soln

--- NeetCode 150/Two Pointers/3sum.py
+++ NeetCode 150/Two Pointers/3sum.py
@@ -55,3 +55,29 @@ def threeSum(nums: list[int]) -> list[list[int]]:
                 while nums[l] == nums[l - 1] and l < r:
                     l += 1
     return output
+
+
+# Summer 2024 solution (still don't understand this too well. come back)
+def threeSum(self, nums: list[int]) -> list[list[int]]:
+
+        ans = []
+        nums.sort()
+
+        for i, num in enumerate(nums):
+
+            if i > 0 and num == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                three_sum = num + nums[l] + nums[r]
+                if three_sum > 0:
+                    r -= 1
+                elif three_sum < 0:
+                    l += 1
+                else:
+                    ans.append([num, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return ans
